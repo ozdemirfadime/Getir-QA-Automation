3c43da35c0ff9250c56a86b0c69a15d358c49328
ozdemirfadime <43369173+ozdemirfadime@users.noreply.github.com>
2021-09-08 17:52:51 +0300

    Last Coding

--- SalaryCalculate.java
+++ SalaryCalculate.java
@@ -1,13 +1,15 @@
-
 import java.util.Scanner;
 public class SalaryCalculate{
     public static void main(String[] args) {
+
         // Base salary must be at least 1000 euros.
         // The weekly working hour standard is 40 hours per week.
         // If the employee has worked less than 40 hours, her/his salary should be reduced proportionally
         // For every hour worked on weekdays over 40 hours, 1.5 times the hourly unit wage is paid.
         //For every hour worked on weekends over 40 hours, 2 times the hourly unit wage is paid
-        // If the employee has worked at least 40 hours during the week and more than 10 hours on the weekend, he or she will receive an additional bonus of $500.
+        // If the employee has worked at least 40 hours during the week and more than 10 hours on the weekend,
+        // he or she will receive an additional bonus of $500.
+
         System.out.println("Enter the base salary ");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the hour working on weekdays");
