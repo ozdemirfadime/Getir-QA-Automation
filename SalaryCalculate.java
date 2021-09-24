dea831b3dc77e0fa519e9cbf086680a1df489d3f
ozdemirfadime <43369173+ozdemirfadime@users.noreply.github.com>
2021-09-24 17:37:36 +0300

    Add files via upload
    fixed code

--- SalaryCalculate.java
+++ SalaryCalculate.java
@@ -1,3 +1,5 @@
+package Week1;
+
 import java.util.Scanner;
 public class SalaryCalculate{
     public static void main(String[] args) {
@@ -12,44 +14,50 @@ public class SalaryCalculate{
 
         System.out.println("Enter the base salary ");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the hour working on weekdays");
-        System.out.println("Enter the hour working on  weekends ");
         double baseSalary= scanner.nextDouble();
-        double weekendsHours   =  scanner.nextDouble();
-        double weekdaysHours   =  scanner.nextDouble();
-        double weekPerHours= weekdaysHours+weekendsHours;
-        double amountPerHour= baseSalary/40;
         double promotedSalary = 0;
-        double amountWeekend;
-        double amountWeekday;
         scanner.close();
 
         if (1000 > baseSalary) {
             System.out.println("Base salary must be at least 1000£");
         }
-        if(weekPerHours>40){
-            amountWeekend = amountPerHour*2*weekendsHours;
-            amountWeekday = amountPerHour*1.5*weekdaysHours;
-            promotedSalary= baseSalary+amountWeekend+amountWeekday;
+        else {
+            double amountPerHour= baseSalary/40;
+
+            System.out.println("Enter the hour working on weekdays");
+            double weekdaysHours   =  scanner.nextDouble();
+
+            System.out.println("Enter the hour working on  weekends ");
+            double weekendsHours   =  scanner.nextDouble();
+
+            double totalWorkingHours = weekdaysHours+weekendsHours;
+
+              if (totalWorkingHours <= 40) {
+                promotedSalary = baseSalary - (40 - totalWorkingHours) * amountPerHour;
+            }
+              else{
+                if (weekdaysHours<40) {
+                    promotedSalary =  baseSalary - ((totalWorkingHours - 40) * amountPerHour * 2) ;
+                }
+
+                else{
+                      if ( weekendsHours <10 ) {
+                    promotedSalary =  baseSalary + ((weekdaysHours - 40) * amountPerHour * 1.5) +
+                            (weekendsHours*amountPerHour * 2);
+                      }
+                      else
+                      {
+                    promotedSalary = baseSalary + ((weekdaysHours - 40) * amountPerHour * 1.5) +
+                            (weekendsHours * amountPerHour * 2) + 500 ;
+                          System.out.println("You earned bonus 500$ ");
+
+
+                }
+              }
+            }
+    }
+        if(baseSalary != 0) {
+            System.out.println("The  promoted salary is:" + promotedSalary);
         }
-        else if (weekPerHours<40){
-            amountWeekend = amountPerHour*weekendsHours;
-            amountWeekday = amountPerHour*weekdaysHours;
-            promotedSalary = baseSalary-(amountWeekend+amountWeekday);
-        }
-        else if((weekPerHours>40)&&(weekendsHours>=10)){
-            amountWeekend = amountPerHour*2*weekendsHours;
-            amountWeekday = amountPerHour*1.5*weekdaysHours;
-            promotedSalary=baseSalary+(amountWeekend+amountWeekday)+500;
-        }
-        else{
-            promotedSalary=baseSalary;
-        }
-        System.out.println("Promoted salary "+ promotedSalary);
-
-
-
-
-
     }
 }
