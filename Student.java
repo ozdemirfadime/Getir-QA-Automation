96dcefcf33ad6233925c7978f9a271db0dcb7b59
ozdemirfadime <43369173+ozdemirfadime@users.noreply.github.com>
2021-10-11 21:18:08 +0300

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,17 @@
+package Week3;
+
+public class Student extends Person {
+    private int year ;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+
+    }
+
+
+
+}
